6cf758de2b50aad10cad85272f6780c08818f8ac
meddkim <meddkim@gmail.com>
2017-03-13 19:56:35 +0800

    eureka

---
+++ spring-cloud-demo-eureka/src/main/java/spring/cloud/base/App.java
@@ -0,0 +1,18 @@
+package spring.cloud.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class App 
+{
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
