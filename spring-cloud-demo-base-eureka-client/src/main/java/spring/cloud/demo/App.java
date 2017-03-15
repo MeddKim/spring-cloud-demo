c6564c1eb51e6ae9d8cd2c1290c99716ec1be0c1
meddkim <meddkim@gmail.com>
2017-03-15 18:44:39 +0800

    add file

--- spring-cloud-demo-base-eureka-client/src/main/java/spring/cloud/demo/App.java
+++ spring-cloud-demo-base-eureka-client/src/main/java/spring/cloud/demo/App.java
@@ -1,16 +1,16 @@
-package spring.cloud.base;
+package spring.cloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-public class App 
+@EnableEurekaServer
+public class App
 {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
---
+++ spring-cloud-demo-eureka-server/src/main/java/spring/cloud/base/App.java
@@ -0,0 +1,19 @@
+package spring.cloud.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class App 
+{
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(App.class, args);
+    }
+}
