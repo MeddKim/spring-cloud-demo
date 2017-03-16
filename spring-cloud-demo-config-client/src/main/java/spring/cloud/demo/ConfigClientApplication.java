b09b1b3b2cd44bc043155c51c1918f37ba2253da
meddkim <meddkim@gmail.com>
2017-03-16 18:37:57 +0800

    add config client

---
+++ spring-cloud-demo-config-client/src/main/java/spring/cloud/demo/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package spring.cloud.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+@SpringBootApplication
+public class ConfigClientApplication
+{
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+    }
+}
