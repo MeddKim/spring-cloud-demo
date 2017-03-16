207a20cbf3ac994a3dca9c120789eb9572caff42
meddkim <meddkim@gmail.com>
2017-03-16 19:20:40 +0800

    add zuul demo

---
+++ spring-cloud-demo-api-gateway/src/main/java/spring/cloud/demo/APIGatewayApplication.java
@@ -0,0 +1,13 @@
+package spring.cloud.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class APIGatewayApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
