f18c50a9fed48ef6d28bc69477c3810aaf53f6fc
Meddkim <meddkim@gmail.com>
2017-11-25 22:07:55 +0800

    a

---
+++ spring-cloud-auth-center/src/main/java/com/demo/security/config/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.demo.security.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Administrator
+ * @date : 2017/3/16 0016
+ * @Description:
+ */
+@RefreshScope
+@RestController
+public class TestController {
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping(value = "/from")
+    public String from(){
+        return this.from;
+
+    }
+}
